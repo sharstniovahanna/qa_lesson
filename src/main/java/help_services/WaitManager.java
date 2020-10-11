4df829123e876f208196a85df4115f00e8c7c525
hsharstniova <sharstniovahanna@gmail.com>
2020-10-11 23:07:26 +0300

    Add extension from AbstractPage to WaitManager

--- src/main/java/help_services/WaitManager.java
+++ src/main/java/help_services/WaitManager.java
@@ -5,11 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class WaitManager {
-    private final WebDriver driver;
+public class WaitManager extends AbstractPage{
 
     public WaitManager(WebDriver driver) {
-        this.driver = driver;
+        super.driver = driver;
     }
 
     public void waitUntilpresenceOfElementLocated(By locator){
